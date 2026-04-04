2e0e3ae720d26068f2efe22dfcccaf30c859ae71
Ajay-Kumar-13 <apulleti@gitam.in>
2026-04-04 23:06:47 +0530

    fetch leads

--- src/main/java/com/crm/leads/service/LeadService.java
+++ src/main/java/com/crm/leads/service/LeadService.java
@@ -23,4 +23,23 @@ public class LeadService {
                 .then(Mono.just(ResponseEntity.status(201).body("Successfully Uploaded all the Leads!")));
     }
 
+    public Mono<List<Lead>> getAllLeads() {
+        return leadRepository.findAll()
+//                .map(lead -> convertToDTO(lead))
+                .map(this::convertToDTO)
+                .collectList();
+    }
+
+    public Lead convertToDTO(com.crm.leads.model.Lead lead) {
+        Lead l = new Lead();
+        l.setName(lead.getName());
+        l.setEmail(lead.getEmail());
+        l.setAddress(lead.getAddress());
+        l.setLeadSource(lead.getLeadSource());
+        l.setLeadState(lead.getLeadState());
+        l.setLeadSubSource(lead.getLeadSubSource());
+        l.setPhone(lead.getPhone());
+        l.setLeadType(lead.getLeadType());
+        return l;
+    }
 }
