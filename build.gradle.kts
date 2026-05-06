837c09e2cae7c408607edea05127787acbf5a568
Ajay-Kumar-13 <apulleti@gitam.in>
2026-05-06 20:59:08 +0530

    dev configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,15 +21,30 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     //	Lombok
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
     // Reactive database access
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.postgresql:r2dbc-postgresql:1.0.4.RELEASE")
 
+    // JDBC ONLY for schema.sql + data.sql initialization
+    runtimeOnly("org.postgresql:postgresql")
+
     //	Spring Security
     implementation("org.springframework.boot:spring-boot-starter-security")
+
+    //  AWS
+    implementation("software.amazon.awssdk:sts")
+    implementation(platform("software.amazon.awssdk:bom:2.25.28"))
+    implementation("software.amazon.awssdk:secretsmanager")
+    implementation("io.awspring.cloud:spring-cloud-aws-starter-secrets-manager:3.1.1")
+
+    //  Flyway
+    implementation("org.flywaydb:flyway-core:11.20.0")
+    implementation("org.flywaydb:flyway-database-postgresql")
 
     //	JWT
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
